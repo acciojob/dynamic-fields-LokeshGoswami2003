dada2508d30f390dd68d25cf5c6b3020159e4d3c
Lokesh Goswami <lokesh.goswami.2003@gmail.com>
2025-11-02 11:09:15 +0530

    comopleted

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,50 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [inputs, setInputs] = useState([{ name: "", age: "" }]);
+
+  const handleAdd = () => {
+    setInputs([...inputs, { name: "", age: "" }]);
+  };
+
+  const handleRemove = (index) => {
+    const newInputs = inputs.filter((_, i) => i !== index);
+    setInputs(newInputs);
+  };
+
+  const handleChange = (index, e) => {
+    const newInputs = [...inputs];
+    newInputs[index][e.target.name] = e.target.value;
+    setInputs(newInputs)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <form >
+        {inputs.map((input, index) => {
+          return (
+            <div key={index}>
+              <input placeholder="Name" name="name" value={input.name} onChange={(e) => handleChange(index, e)}/>
+              <input placeholder="Age" name="age" value={input.age} onChange={(e) => handleChange(index, e)}/>
+              <button type="button" onClick={() => handleRemove(index)}>
+                Remove
+              </button>
+            </div>
+          );
+        })}
+      </form>
+      <button onClick={handleAdd}> Add More.. </button>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+      <p>After Clicking submit check console for data</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
